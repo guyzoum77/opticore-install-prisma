537a921d1b94b0567d0f622b989a970cef476f9f
guyzoum77 <guyzoum7700@mail.com>
2024-05-21 18:12:42 +0000

    adding loader after installation

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,10 @@ import clackCLI from "@clack/prompts";
 import colors from "ansi-colors";
 import {promisify} from "util";
 import cp from "child_process";
+import fs from "fs";
 
-export async function initializePrismaFunction(projectName: string, provider: string | any) {
+export async function initializePrismaFunction(projectName: string, provider: string | any, projectPath: any): Promise<void> {
+    let ora = (await import("ora")).default;
     const asyncExec = promisify(cp.exec);
 
     const installPrismaORM =  await clackCLI.select({
@@ -19,17 +21,21 @@ export async function initializePrismaFunction(projectName: string, provider: st
 
     if (clackCLI.isCancel(installPrismaORM)) {
         console.log(`${colors.bgRed(`${colors.white("Operation cancelled.")}`)}`);
+        fs.rmSync(projectPath, { recursive: true, force: true });
         process.exit(0);
     }
 
     if (installPrismaORM[0] === "prisma_orm") {
+        const prismaORMSpinner = ora("Prisma ORM installation and configuration").start();
         await asyncExec("npm install prisma --save-dev @prisma/client").then(async () => {
             try {
                 await asyncExec(`npx prisma init --datasource-provider ${provider}`, { cwd: projectName });
-            } catch (error) {
-                console.error("Error executing command:", error);
+            } catch (error: any) {
+                console.error("Error executing command:", error.message);
+                fs.rmSync(projectPath, { recursive: true, force: true });
             }
         });
+        prismaORMSpinner.succeed();
 
     } else {
         console.log(colors.bgCyan(`${colors.white(`You did not choose prisma as your ORM.`)}`));
